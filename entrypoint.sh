7d3c109e9deeb6cfa1fbfc47f3699bb3a11374a8
Hannes De Valkeneer <2261239+hdevalke@users.noreply.github.com>
2024-08-17 12:24:53 +0200

    fix: fix download urls

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,28 @@ set -e
 os=$(echo "$RUNNER_OS" | tr '[:upper:]' '[:lower:]')
 convco_version="$3"
 
+if [ $convco_version = "latest" ]; then
+    base_url="https://github.com/convco/convco/releases/latest/download"
+else
+    base_url="https://github.com/convco/convco/releases/download/$convco_version"
+fi
+
 case "$os" in
   macos)
-    wget https://github.com/convco/convco/releases/download/$convco_version/convco-macos.zip -O convco.zip
+    wget "$base_url/convco-macos.zip" -O convco.zip
     unzip convco.zip
     rm convco.zip
     mv convco ./
     chmod +x ./convco
     ;;
   windows)
-    wget https://github.com/convco/convco/releases/download/$convco_version/convco-windows.zip -O convco.zip
+    wget "$base_url/convco-windows.zip" -O convco.zip
     unzip convco.zip
     rm convco.zip
     mv convco.exe ./convco.exe
     ;;
   *)
-    wget https://github.com/convco/convco/releases/download/$convco_version/convco-ubuntu.zip -O convco.zip
+    wget "$base_url/convco-ubuntu.zip" -O convco.zip
     unzip convco.zip
     rm convco.zip
     mv convco ./convco
